67972afcc4280a36a304109eb3d259066ca59f29
Will Carter <willmckenna64@gmail.com>
2018-08-06 19:14:12 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def person=(persons_name)
+    @name = persons_name
+  end
+  
+  def person
+    @name
+  end
+  
+  def job=(persons_job)
+    @job = persons_job
+  end
+  
+  def job
+    @job
+  end
+end
