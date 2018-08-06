67af211159eca12f1203bac6b97ec38f2548f224
Will Carter <willmckenna64@gmail.com>
2018-08-06 19:05:11 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,17 @@
 class Dog
   
-  def name
-    @name
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
   
-  def name=(dog_name)
-    @name = dog_name
+  def name
+    @this_dogs_name
+  end
+  
+  def breed=(dogs_breed)
+    @this_dogs_breed = dogs_breed
   end
   
   def breed
-    @breed
-  end
-  
-  def breed=(dog_breed)
-    @breed = dog_breed
+    @this_dogs_breed
   end
